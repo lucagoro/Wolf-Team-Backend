f4b98aa5a8ac527e5a5bcee709e69abc8b143996
Luca Gorosito <lucagoro04@gmail.com>
2026-01-05 12:30:27 -0300

    cors

--- src/main/java/com/mma/gestion/config/CorsConfig.java
+++ src/main/java/com/mma/gestion/config/CorsConfig.java
@@ -5,13 +5,17 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer{
-    
+public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                    "http://localhost:5173",
+                    "https://wolf-team-psi.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
